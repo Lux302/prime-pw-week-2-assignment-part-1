4e282198a300d6e28a40e056f2bb4b66c95b9dcd
Frank Raso <frankraso91@gmail.com>
2022-03-12 20:44:14 -0500

    finaltouches

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,18 +1,18 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-const  firstName = 'Frank';
+let  firstName = 'Frank';
 console.log(firstName);
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-const lastName = 'Raso';
+let lastName = 'Raso';
 console.log(lastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-const fullName = 'Frank Raso';
+let fullName = 'Frank Raso';
 console.log(fullName);
 // 4 - Console log the value of `fullName`
 console.log(fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-const luckyNumber = 21;
+let luckyNumber = 21;
 console.log(luckyNumber);
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
